77053d75710b2b30d961ecfdd6a426dccd336f84
Julien Dewost <dewostjulien@gmail.com>
2020-02-04 12:02:38 +0100

    composant tp2

---
+++ src/app/collegue/collegue.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Collegue } from '../models/Collegue';
+
+@Component({
+  selector: 'app-collegue',
+  templateUrl: './collegue.component.html',
+  styleUrls: ['./collegue.component.css']
+})
+export class CollegueComponent implements OnInit {
+  @Input() col: Collegue;
+  constructor() { }
+  modifier = true;
+  ngOnInit() {
+  }
+  ajouterCollegue() {
+
+    console.log('Création d\'un nouveau collègue');
+
+  }
+
+  modifierCollegue(col: Collegue) {
+    this.modifier = false;
+    console.log(`modification du collègue ${col.nom}`);
+
+  }
+
+}
---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,9 @@
+import {Collegue} from "../models/Collegue";
+
+
+
+let collegueMock: Collegue = new Collegue('M01', 'Dupont', 'Paul', 'paul.dupont@gmail.com', new Date(1990, 4, 5), 'toto');
+
+
+
+export {collegueMock};
