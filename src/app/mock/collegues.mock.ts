439d44795224e5c3900f980edc29aa0815a6468e
Julien Dewost <dewostjulien@gmail.com>
2020-02-04 13:56:09 +0100

    composant tp2 maj

--- src/app/mock/collegues.mock.ts
+++ src/app/mock/collegues.mock.ts
@@ -2,7 +2,7 @@ import {Collegue} from "../models/Collegue";
 
 
 
-let collegueMock: Collegue = new Collegue('M01', 'Dupont', 'Paul', 'paul.dupont@gmail.com', new Date(1990, 4, 5), 'toto');
+let collegueMock: Collegue = new Collegue('M01', 'Regis', 'toto', 'regis.toto@gmail.com', new Date(1987, 2, 1), 'http://www.w3schools.com/bootstrap/img_avatar3.png');
 
 
 
