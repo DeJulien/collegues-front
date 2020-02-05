221f340f00d0611b46caf6c116d21cae882ca61d
Julien Dewost <dewostjulien@gmail.com>
2020-02-05 10:01:12 +0100

    tp3 maj1

---
+++ src/app/mock/matricules.mock.ts
@@ -0,0 +1,16 @@
+import { Collegue } from '../models/Collegue';
+
+
+
+const listeMatricules: Collegue[] = [new Collegue('M01', 'Regis', 'Toto', 'regis.toto@gmail.com',
+new Date(1990, 1, 1), 'http://www.w3schools.com/bootstrap/img_avatar3.png'),
+
+new Collegue('M03', 'Jule', 'Cesar', 'jule.cesar@gmail.com',
+new Date(1902, 5, 5), 'http://www.w3schools.com/bootstrap/img_avatar3.png'),
+
+new Collegue('M04', 'Sarah', 'Croche', 'sarah.croche@gmail.com',
+new Date(1910, 10, 10), 'http://www.w3schools.com/bootstrap/img_avatar3.png')];
+
+
+
+export {listeMatricules};
